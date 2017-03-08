690bbfebd49d9710a9e56b18a7870a748d61bfe1
Darren Leung <darren.leung@gatech.edu>
2017-03-08 15:33:04 -0500

    Java header and packet classes

---
+++ RTPHeader.java
@@ -0,0 +1,146 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+import java.util.Arrays;
+
+	public class RTPHeader {
+		private int srcPort;
+		private int dstPort;
+		private int seqNum;
+		private int windowSizeOffset;
+		private int checksum;
+		
+		public int getSrcPort() {
+			return srcPort;
+		}
+
+		public void setSrcPort(int srcPort) {
+			this.srcPort = srcPort;
+		}
+
+		public int getDstPort() {
+			return dstPort;
+		}
+
+		public void setDstPort(int dstPort) {
+			this.dstPort = dstPort;
+		}
+
+		private boolean ACK;
+		private boolean NACK;
+		private boolean SYN;
+		private boolean FIN;
+		private boolean BEG;
+		
+		private int timestamp;
+		
+		public RTPHeader() {
+			this.windowSizeOffset = 0;
+			this.checksum = 0;
+			this.ACK = false;
+			this.NACK = false;
+			this.SYN = false;
+			this.FIN = false;
+			this.BEG = false;
+			this.timestamp = 0;
+		}
+		
+		public RTPHeader(int srcPort, int dstPort, int seqNum) {
+			this();
+			this.srcPort = srcPort;
+			this.dstPort = dstPort;
+			this.seqNum = seqNum;
+		}
+		
+		public void setseqNum(int seqNum) { this.seqNum = seqNum; }
+		
+		public int getseqNum() { return seqNum; }
+		
+		public void setWindowSizeOffset(int windowSizeOffset) { this.windowSizeOffset = windowSizeOffset; }
+		
+		public int getWindowSizeOffset() { return windowSizeOffset; }
+		
+		public boolean isACK() { return ACK; }
+
+		public void setACK(boolean ACK) { this.ACK = ACK; }
+
+		public boolean isNACK() { return NACK; }
+
+		public void setNACK(boolean NACK) { this.NACK = NACK; }
+
+		public boolean isSYN() { return SYN; }
+
+		public void setSYN(boolean SYN) { this.SYN = SYN; }
+
+		public boolean isFIN() { return FIN; }
+
+		public void setFIN(boolean FIN) { this.FIN = FIN; }
+
+		public boolean isBEG() { return BEG; }
+
+		public void setBEG(boolean BEG) { this.BEG = BEG; }
+
+		public void setChecksum(int checksum) { this.checksum = checksum; }
+		
+		public int getChecksum() { return checksum; }
+		
+		public byte[] getHeaderByteArray() {
+			byte[] headerByteArray;
+
+			ByteBuffer byteBuffer = ByteBuffer.allocate(Integer.BYTES * 7);
+			byteBuffer.order(ByteOrder.BIG_ENDIAN);
+
+			byteBuffer.putInt(srcPort);
+			byteBuffer.putInt(dstPort);
+			byteBuffer.putInt(seqNum);
+			byteBuffer.putInt(windowSizeOffset);
+			byteBuffer.putInt(checksum);
+
+			int ackByte = (ACK ? 1 : 0) << 31;
+			int nackByte = (NACK ? 1 : 0) << 30;
+			int synByte = (SYN ? 1 : 0) << 29;
+			int finByte = (FIN ? 1 : 0) << 28;
+			int begByte = (BEG ? 1 : 0) << 27;
+			
+
+			int flags = ackByte | nackByte | synByte | finByte | begByte;
+			byteBuffer.putInt(flags);
+			byteBuffer.putInt(timestamp);
+			headerByteArray = byteBuffer.array();
+			return headerByteArray;
+		}
+		
+		public RTPHeader(byte[] headerByteArray) {
+			ByteBuffer byteBuffer = ByteBuffer.wrap(headerByteArray);
+			IntBuffer intBuffer = byteBuffer.asIntBuffer();
+			this.srcPort = intBuffer.get(0);
+			this.dstPort = intBuffer.get(1);
+			this.seqNum = intBuffer.get(2);
+			this.windowSizeOffset = intBuffer.get(3);
+			this.checksum = intBuffer.get(4);
+			int flags = intBuffer.get(5);
+			this.timestamp = intBuffer.get(6);
+			
+			int ackInt = (flags >>> 31) & 0x1;
+			int nackInt = (flags >>> 30) & 0x1;
+			int synInt = (flags >>> 29) & 0x1;
+			int finInt = (flags >>> 28) & 0x1;
+			int begInt = (flags >>> 27) & 0x1;
+			
+			this.ACK = (ackInt != 0);
+			this.NACK = (nackInt != 0);
+			this.SYN = (synInt != 0);
+			this.FIN = (finInt != 0);
+			this.BEG = (begInt != 0);
+		}
+		
+		public int getTimestamp() {
+			return timestamp;
+		}
+		
+		public void setTimestamp(int timestamp) {
+			this.timestamp = timestamp;
+		}
+	}
---
+++ RTPPacket.java
@@ -0,0 +1,84 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
+
+
+public class RTPPacket {
+
+	private RTPHeader header;
+
+	private byte[] data;
+
+	public RTPPacket(int sourcePort, int destinationPort, byte[] data) {
+		this.setHeader(new RTPHeader(sourcePort, destinationPort, 0));
+		this.setData(data);
+	}
+
+	public RTPPacket(int sourcePort, int destinationPort) {
+		this.setHeader(new RTPHeader(sourcePort, destinationPort, 0));
+		this.setData(data);
+	}
+
+	public RTPPacket(RTPHeader header, byte[] data) {
+		this.setHeader(header);
+		this.setData(data);
+	}
+
+	public RTPPacket(byte[] packetByteArray) {
+		byte[] headerBytes = Arrays.copyOfRange(packetByteArray, 0, 28);
+		this.setHeader(new RTPHeader(headerBytes));
+
+		if (packetByteArray.length > 28) {
+			byte[] dataBytes = Arrays.copyOfRange(packetByteArray, 28, packetByteArray.length);
+			this.setData(dataBytes);
+		}
+	}
+	
+	public byte[] getPacketByteArray() {
+		byte[] packetByteArray;
+		byte[] headerByteArray;
+		
+		headerByteArray = header.getHeaderByteArray();
+		
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		try {
+			outputStream.write(headerByteArray);
+			if (data != null) {
+				outputStream.write(data);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		packetByteArray = outputStream.toByteArray();
+		return packetByteArray;
+	}
+	
+	public RTPHeader getHeader() { return header; }
+
+	public void setHeader(RTPHeader header) { this.header = header; }
+
+	public byte[] getData() { return data; }
+
+	public void setData(byte[] data) { this.data = data; }
+
+	public void updateChecksum() { header.setChecksum(calculateChecksum()); }
+	
+	public int calculateChecksum() {
+		int checksumValue;
+		
+		CRC32 checksum = new CRC32();
+		
+		byte[] packetByteArray = getPacketByteArray();
+
+		packetByteArray[16] = 0x00;
+		packetByteArray[17] = 0x00;
+		packetByteArray[18] = 0x00;
+		packetByteArray[19] = 0x00;
+
+		checksum.update(packetByteArray);
+		checksumValue = (int) checksum.getValue();
+		return checksumValue;
+	}
+}
