68785e1c8fd39268c880f08641155c6803a932be
Jason Wu <jasonwupilly1997@gmail.com>
2017-03-13 23:43:34 -0400

    make python client serialize class into bytes

---
+++ out/production/CS3251HW2Project/RTPPacket.py
@@ -0,0 +1,19 @@
+#this is the packet class, to be used to store data and checksum value
+class RTPPacket:
+	#sets up the packet
+	#If packet is corrupt ack is 1, else ack is 0
+	def __init__(self,header,data):
+		self.data = data #list/array of bytes
+		self.header = header
+
+	def getData(self):
+		return self.data
+
+	def getHeader(self):
+		return self.header
+
+	def setData(self,d):
+		self.data = d
+
+	def setHeader(self,v):
+		self.header = v
